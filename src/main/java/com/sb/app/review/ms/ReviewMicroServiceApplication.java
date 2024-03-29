c55a8f039d3112920452f32b0de66119392c7152
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-29 22:57:32 +0530

    feign added for zipkin tracing

--- src/main/java/com/sb/app/review/ms/ReviewMicroServiceApplication.java
+++ src/main/java/com/sb/app/review/ms/ReviewMicroServiceApplication.java
@@ -2,8 +2,10 @@ package com.sb.app.review.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ReviewMicroServiceApplication {
 
 	public static void main(String[] args) {
